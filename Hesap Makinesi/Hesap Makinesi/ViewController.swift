3ac6baed07112734fc2e190fe19647e5571cb3d0
yas373 <yasin_turpcu_67@hotmail.com>
2018-08-13 17:59:12 +0300

    Hesap makinesi ilk 3 sayının eklenmiş hali

---
+++ Hesap Makinesi/Hesap Makinesi/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Hesap Makinesi
+//
+//  Created by Yasin Turpcu on 13.08.2018.
+//  Copyright © 2018 Yasin Turpcu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
